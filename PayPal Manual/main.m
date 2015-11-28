5b4dee24b676862e6296aea1424b0b6a2e862024
Rick <rvcuag@hotmail.com>
2015-11-28 12:04:48 -0600

    Initial Commit

---
+++ PayPal Manual/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayPal Manual
+//
+//  Created by Ricardo Vera on 28/11/15.
+//  Copyright © 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
